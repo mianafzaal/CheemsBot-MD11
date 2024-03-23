65d4f948d454f93ea043873fa2fa34b08e215126
mian <96155069+mianafzaal@users.noreply.github.com>
2024-03-23 22:59:32 +0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -14,36 +14,36 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: M" //ur yt chanel name
+global.socialm = "GitHub: Mian" //ur github or insta name
+global.location = "Pakistan, Lahore" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.botname = '𝑪𝒉𝒆𝒆𝒎𝒔 𝑩𝒐𝒕 𝑴𝑫 𝑽11' //ur bot name
+global.ownernumber = '923430436852' //ur owner number
+global.ownername = '©᭄𝑴𝒊𝒂𝒏🌸' //ur owner name
+global.websitex = "+923430436852"
+global.wagc = "https://www.instagram.com/its_mian__07/"
+global.themeemoji = '👿'
+global.wm = "Mian Bot Inc."
+global.botscript = 'https://Wa.me//923430436852' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "©᭄𝑴𝒊𝒂𝒏🌸ོ\n\n+923430436852"
+global.creator = "923430436852"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["923430436852"] // Premium User
 global.hituet = 0
 
 //bot sett
 global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.autoblocknumber = '212' //set autoblock country code
+global.antiforeignnumber = '972' //set anti foreign number country code
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
 global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
